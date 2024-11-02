4cf15c2851ad42d5dc4831b5aff688158f91c144
amnesia2k <tilewaolatoye17@gmail.com>
2024-11-02 21:52:28 +0100

    finished modal project

--- pop-up-modal/script.js
+++ pop-up-modal/script.js
@@ -1,3 +1,31 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  // console.log('key pressed');
+  // console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
